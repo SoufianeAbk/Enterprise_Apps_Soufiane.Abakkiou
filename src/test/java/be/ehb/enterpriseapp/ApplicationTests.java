e703f1bdc62360a892c42209d554645a2501a472
SoufianeAbk <229384282+sso-vbk@users.noreply.github.com>
2026-05-28 18:47:53 +0200

    Initial Commit + Dependencies.

---
+++ src/test/java/be/ehb/enterpriseapp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.enterpriseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
